27154239d4023470f5b707645888d7aeb463deeb
Francis Chen <fncis.chen@gmail.com>
2019-03-28 09:58:54 -0700

    adding data for ingreidents table

--- migrations/20190328094839_create_ingredients_table.js
+++ migrations/20190328094839_create_ingredients_table.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable('ingredients', function(tbl){
+      tbl.increments();
+      tbl.string("name")
+      .unique();
+      
+    })
+  };
   
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+  exports.down = function(knex, Promise) {
+      return knex.schema.dropTableIfExists('ingredients');
+  };
