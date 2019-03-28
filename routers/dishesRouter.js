74cb7b851e6f1752ba7cbc37fc20c7a561c0abbf
Francis Chen <fncis.chen@gmail.com>
2019-03-28 12:08:54 -0700

    added server router endpoints for dishes and recipes

--- routers/dishesRouter.js
+++ routers/dishesRouter.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const dishes = require("../data/dishesDataModel/dishesDataModel.js")
 
+router.get("/api/dishes", (req,res) => {
 
+})
+
+router.get("/api/dishes/:id", (req,res) => {
+    
+})
+
+router.post("/api/dishes", (req,res) => {
+    
+})
 
 module.exports = router; 
--- routers/recipesRouter.js
+++ routers/recipesRouter.js
@@ -2,4 +2,13 @@ const router = require('express').Router();
 const recipies = require("../data/recipiesDataModel/recipiesDataModel.js")
 
 
+router.get("/api/recipes", (req,res) => {
+
+})
+
+router.post("/api/recipes/", (req,res) => {
+    
+})
+
+
 module.exports = router; 
