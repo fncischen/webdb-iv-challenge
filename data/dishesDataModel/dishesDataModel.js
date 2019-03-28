de1f4b0e0753f47951c912217860087a33082698
Francis Chen <fncis.chen@gmail.com>
2019-03-28 09:27:29 -0700

    added data model functions for dishes and recipies

--- data/dishesDataModel/dishesDataModel.js
+++ data/dishesDataModel/dishesDataModel.js
@@ -1,4 +1,19 @@
 const db = require('../data/dbConfig.js');
 
 module.exports = {
+    getDishes,
+    addDish,
+    getDish 
+}
+
+function getDishes() {
+
+}
+
+function addDish(dish) {
+    
+}
+
+function getDish(id) {
+    
 }
--- data/recipiesDataModel/recipiesDataModel.js
+++ data/recipiesDataModel/recipiesDataModel.js
@@ -1,4 +1,14 @@
 const db = require('../data/dbConfig.js');
 
 module.exports = {
+    getRecipies, 
+    addRecipie
+}
+
+function getRecipies() {
+
+}
+
+function addRecipie() {
+    
 }
