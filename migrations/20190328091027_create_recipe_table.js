8788aa2d82ff36aaf51f36df9b8110526182fb2c
Francis Chen <fncis.chen@gmail.com>
2019-03-28 09:46:03 -0700

    added migrations for recipies

--- migrations/20190328091027_create_recipe_table.js
+++ migrations/20190328091027_create_recipe_table.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  
-};
+    return knex.schema.createTable('recipes', function(tbl){
+      tbl.increments();
+      tbl.string("dish-name")
+      .notNullable().references("name").inTable("dishes")
+      
+      tbl.string("recipe-name", 50)
+      .notNullable()
+      .unique();
 
-exports.down = function(knex, Promise) {
+    })
+  };
   
-};
+  exports.down = function(knex, Promise) {
+      return knex.schema.dropTableIfExists('recipes');
+  };
