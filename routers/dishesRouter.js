d751306870f503a7b5ba5f5f9df7037a753de8ec
Francis Chen <fncis.chen@gmail.com>
2019-03-28 12:57:39 -0700

    filled out server endpoints for dish router and recipe router

--- routers/dishesRouter.js
+++ routers/dishesRouter.js
@@ -2,15 +2,21 @@ const router = require('express').Router();
 const dishes = require("../data/dishesDataModel/dishesDataModel.js")
 
 router.get("/api/dishes", (req,res) => {
-
+    dishes.getDishes()
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json(err));
 })
 
 router.get("/api/dishes/:id", (req,res) => {
-    
+    dishes.getDish(req.params.id)
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json(err));
 })
 
 router.post("/api/dishes", (req,res) => {
-    
+    dishes.addDish(req.body)
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json(err));
 })
 
 module.exports = router; 
--- routers/recipesRouter.js
+++ routers/recipesRouter.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
-const recipies = require("../data/recipiesDataModel/recipiesDataModel.js")
+const recipes = require("../data/recipiesDataModel/recipiesDataModel.js")
 
 router.get("/api/recipes", (req,res) => {
-
+    recipes.getRecipes()
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json(err));
 })
 
 router.post("/api/recipes/", (req,res) => {
-    
+    recipes.post(req.body)
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json(err));
 })
 
 
