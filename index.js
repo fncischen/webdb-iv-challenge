4efbbe97241c73528a5314b142c5c457149feefd
Francis Chen <fncis.chen@gmail.com>
2019-03-28 10:12:13 -0700

    set up routing system for dishes & routers

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const express = require("express");
 const server = express(); 
-const db = require("./data/dbConfig.js")
+
+const dishesRouter = require("./routers/dishesRouter.js")
+const recipesRouter = require("./routers/recipesRouter.js")
+
+server.use('/api/dishes', dishesRouter);
+server.use('/api/recipes', recipesRouter);
+
+const port = 500
+server.listen(port, () => {
+    console.log(`\n *** Listening on ${port} *** \n`);
+})
