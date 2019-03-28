979bd42e16279611a50c081a2b6fab7fff4d7085
Francis Chen <fncis.chen@gmail.com>
2019-03-28 09:39:25 -0700

    added migration data for dishes

--- migrations/20190328091008_create_dishes_table.js
+++ migrations/20190328091008_create_dishes_table.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('dishes', function(tbl){
+    tbl.increments();
+    tbl.string("name")
+    .notNullable()
+    .unique();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('dishes');
 };
